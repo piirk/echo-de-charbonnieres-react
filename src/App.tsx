d931e978f26b8678350bb4b3c4373b603b36de2f
piirk <baptiste.boucharechas@gmail.com>
2025-03-29 15:57:13 +0100

    feat: Remove unused CSS files and configure Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from 'react-router-dom';
-import './App.css';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ConcertsPage from './pages/ConcertsPage';
